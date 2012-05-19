a1eaeb75b9dc0f6100c765a4f1ae17e16a10a69d
Steven Wilkin <stevenwilkin@gmail.com>
2012-05-19 21:26:01 +0100

    port over existing update code from OS X app

--- CoffeeTrackerIOS/ViewController.h
+++ CoffeeTrackerIOS/ViewController.h
@@ -7,11 +7,27 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AFHTTPClient.h"
+#import "AFHTTPRequestOperation.h"
+#import "ApiKey.h"
 
-@interface ViewController : UIViewController
+#ifdef TARGET_IPHONE_SIMULATOR
+    #define API_BASE_URL @"http://0.0.0.0:9292"
+#endif
+
+#ifdef TARGET_OS_IPHONE
+    #undef API_BASE_URL
+    #define API_BASE_URL @"http://coffee-tracker.herokuapp.com"
+#endif
+
+@interface ViewController : UIViewController {
+    AFHTTPClient *client;
+}
 
 @property (nonatomic) IBOutlet UIButton *button;
+@property (assign) IBOutlet UILabel *count;
 
+- (void)setCountFromResponseOperation:(AFHTTPRequestOperation *)operation;
 - (IBAction)trackCoffee:(id)sender;
 
 @end
--- CoffeeTrackerIOS/ViewController.m
+++ CoffeeTrackerIOS/ViewController.m
@@ -11,13 +11,26 @@
 @implementation ViewController
 
 @synthesize button;
+@synthesize count;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     UIImage *image = [UIImage imageNamed:@"btn-bg.png"];
     UIImage *stretchedImage = [image stretchableImageWithLeftCapWidth:6 topCapHeight:0];
     [button setBackgroundImage:stretchedImage forState:UIControlStateNormal];
+    
+    NSURL *url = [NSURL URLWithString:API_BASE_URL];
+    client = [[AFHTTPClient alloc] initWithBaseURL:url];
+    // API key must be set in pre-processor macros
+    [client setDefaultHeader:@"X-API-KEY" value:API_KEY];
+    
+    // set count label
+    [count setText:@""];
+    [client getPath:@"/api" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [self setCountFromResponseOperation:operation];
+    } failure:nil];
 }
 
 - (void)viewDidUnload
@@ -30,8 +43,25 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (void)setCountFromResponseOperation:(AFHTTPRequestOperation *)operation {
+    // convert response body to an integer
+    int value = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:[operation responseString]];
+    [scanner scanInt:&value];
+    NSString *label = [NSString stringWithFormat:@"%d", value];
+    [count setText:label];
+}
+
 - (IBAction)trackCoffee:(id)sender {
-    NSLog(@"> coffee!");
+    // disable button while POST is in progress
+    [button setEnabled:NO];
+    
+    [client postPath:@"/api" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [self setCountFromResponseOperation:operation];
+        [button setEnabled:YES];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [button setEnabled:YES];
+    }];
 }
 
 @end
