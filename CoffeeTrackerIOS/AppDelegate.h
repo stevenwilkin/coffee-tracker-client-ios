7b804e9452567eaf11377a03a748488ffe8f0a7b
Steven Wilkin <stevenwilkin@gmail.com>
2012-05-19 18:20:22 +0100

    Initial Commit

---
+++ CoffeeTrackerIOS/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoffeeTrackerIOS
+//
+//  Created by Steven Wilkin on 19/05/2012.
+//  Copyright (c) 2012 NullTheory Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CoffeeTrackerIOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoffeeTrackerIOS
+//
+//  Created by Steven Wilkin on 19/05/2012.
+//  Copyright (c) 2012 NullTheory Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
